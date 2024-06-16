a2daa2e3928138cf2821f131086a2628aa377aa9
Gurusewak1 <gurusewaksinghbains@gmail.com>
2024-06-16 12:21:54 -0500

    validations are working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
+# app/models/product.rb
 class Product < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  end
+  
